145af06bdde6e31a19f80591de2ce07103610cb0
José Ricardo Mendoza <joseanakin@outlook.es>
2022-11-06 19:08:31 -0600

    Deploy richi_mc

---
+++ src/addFiles/ReadFiles.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package addFiles;
+
+/**
+ *
+ * @author richi_mc
+ */
+public class ReadFiles {
+    private String nombre;
+    
+    public ReadFiles() {
+        nombre = "Joshua";
+    }
+}
